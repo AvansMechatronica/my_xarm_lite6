c789ab659ba48b40bfb5453520464afdb708926c
Gerard Harkema <ga.harkema@avans.nl>
2023-11-19 20:17:42 +0100

    first commit

---
+++ install/install.bash
@@ -0,0 +1,9 @@
+sudo apt install ros-melodic-moveit
+sudo apt install ros-melodic-joint-state-publisher-gui
+sudo apt install ros-melodic-combined-robot-hw
+sudo apt install ros-melodic-moveit-servo
+sudo apt install ros-melodic-moveit-visual-tools
+sudo apt install ros-melodic-ros-controllers
+
+fuel.ignitionrobotics.org
+
