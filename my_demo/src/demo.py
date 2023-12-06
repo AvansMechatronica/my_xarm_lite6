b325f74b54334e6c08417a5c7a199e5b88000efd
Gerard Harkema <ga.harkema@avans.nl>
2023-12-06 19:34:33 +0100

    20231206

--- my_demo/src/demo.py
+++ my_demo/src/demo.py
@@ -9,7 +9,9 @@ from std_msgs.msg import Empty
 import math
 import actionlib
 from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Quaternion
 
+from tf.transformations import *
 
 import tf2_ros
 import tf2_geometry_msgs
@@ -43,13 +45,20 @@ group.set_pose_target(posetarget)
 plan=group.plan()
 group.go(wait=True)
 
-
 print("== go to test IK ==")
 transform = tf_buffer.lookup_transform('world', 'ik_testpoint', rospy.Time())
 
 destination_pose = Pose()
 destination_pose.position = transform.transform.translation
 destination_pose.orientation = transform.transform.rotation
+
+q_orig = [transform.transform.rotation.x, transform.transform.rotation.y, transform.transform.rotation.z, transform.transform.rotation.w]
+q_rot = quaternion_from_euler(math.pi, 0, 0) # rotate along x-axis to meet z-face
+q_new = quaternion_multiply(q_rot, q_orig)
+transform.transform.rotation.x = q_new[0]
+transform.transform.rotation.y = q_new[1]
+transform.transform.rotation.z = q_new[2]
+transform.transform.rotation.w = q_new[3]
 
 group.set_pose_target(destination_pose)
 plan=group.plan()
@@ -66,9 +75,6 @@ group.go(target_values, wait = True)
 print("== go to resting ==")
 target_values= group.get_named_target_values("resting")
 group.go(target_values, wait = True)
-
-
-
 
 print("== ready ==")
 
